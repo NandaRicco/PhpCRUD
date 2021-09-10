ce9a6e97310bbc3bec5c5bbc30d5786d486a4d80
NandaRicco <88094296+NandaRicco@users.noreply.github.com>
2021-09-10 13:32:54 +0700

    Add files via upload

---
+++ perpustakaan/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</head>
+<body>
+<nav class="navbar navbar-expand-sm bg-primary navbar-dark">
+<a class="navbar-brand" href="#">
+    <img src="Kucing.jpg" alt="logo" style="width:40px;">
+  </a>
+  <ul class="navbar-nav">
+    <li class="nav-item active">
+      <a class="nav-link" href="index.php">Home</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="tampil_siswa.php">Data siswa</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="tampil_kelas.php">Data kelas</a>
+  </ul>
+</nav>
+<div class="container-fluid">
+  <h3>Ini adalah halaman index</h3>
+</div>
+
+</body>
+</html>
